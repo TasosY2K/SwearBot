6b30def3bbe91a645267f7a03503b9393b703cee
Lean <gustavolama20@gmail.com>
2019-08-05 01:29:12 +0300

    Fixed typos and added sharding.

---
+++ bot.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const request = require('request');
+const secret = require('./config.json');
+const bot = new Discord.Client();
+
+bot.login(secret.token); //secret token
+
+bot.on('ready', async guild => {
+    bot.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
+    console.log(`${bot.user.username} is ready!\nInvite => ${await bot.generateInvite(["ADMINISTRATOR"])}`);
+});
+
+bot.on("guildCreate", async guild => {
+    bot.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
+    let channel = bot.channels.get(guild.systemChannelID);
+    if (!channel) return;
+    let embed = new Discord.RichEmbed()
+      .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
+      .setTitle(`I am **${bot.user.username}**, and you better watch your mouth while im in **${guild.name}**`)
+      .addField("Made by", "**(Lean)#0108**\nUsing [discord.js](https://discord.js.org) & [request](https://www.npmjs.com/package/request)")
+      .addField("Links", `[GitHub](https://github.com/TasosY2K/SwearBot)\n[Website](https://bot.ly/swear-bot)\n[Invite](${await bot.generateInvite(["ADMINISTRATOR"])})`)
+    channel.send(embed);
+});
+
+bot.on("guildDelete", async guild => {
+  bot.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
+});
+
+bot.on("message", async message => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+
+    request(`https://www.purgomalum.com/service/containsprofanity?text=${message.content.toString()}`, (err, res, body) => {
+      if (body === "true") {
+        request(`https://insult.mattbas.org/api/insult`, (err, res, body) => {
+          message.reply(body);
+        });
+      }
+    });
+
+});
--- index.js
+++ index.js
@@ -1,41 +1,9 @@
 const Discord = require('discord.js');
-const request = require('request');
-const secret = require('./config.json');
+const {ShardingManager} = require('discord.js');
 const bot = new Discord.Client();
 
-bot.login(secret.token); //secret token
+const secret = require('./config.json');
 
-bot.on('ready', async guild => {
-    client.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
-    console.log(`${bot.user.username} is ready!\nInvite => ${await bot.generateInvite(["ADMINISTRATOR"])}`);
-});
+const manager = new ShardingManager("./bot.js", {token: secret.token})
 
-bot.on("guildCreate", async (bot, guild) => {
-    client.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
-    let channel = bot.channels.get(guild.systemChannelID);
-    if (!channel) return;
-    let embed = new Discord.RichEmbed()
-      .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-      .setTitle(`I am **${bot.user.username}**, and you better watch your mouth while im in **${guild.name}**`)
-      .addField("Made by", "**(Lean)#0108**\nUsing [discord.js](https://discord.js.org) & [request](https://www.npmjs.com/package/request)")
-      .addField("Links", `[GitHub](https://github.com/TasosY2K/SwearBot)\n[WebSite](https://bot.ly/swear-bot)\n[Invite](${await bot.generateInvite(["ADMINISTRATOR"])})`)
-    channel.send(embed);
-});
-
-bot.on("guildDelete", async (bot, guild) => {
-  client.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
-});
-
-bot.on("message", async message => {
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-
-    request(`https://www.purgomalum.com/service/containsprofanity?text=${message.content.toString()}`, (err, res, body) => {
-      if (body === "true") {
-        request(`https://insult.mattbas.org/api/insult`, (err, res, body) => {
-          message.reply(body);
-        });
-      }
-    });
-
-});
+manager.spawn('auto', 2000, true);
