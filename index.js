2648f3176f3036c86679d48a9d459d0c305f7e9c
Lean <gustavolama20@gmail.com>
2019-08-04 16:48:21 +0300

    Added guild count and website link.

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ const bot = new Discord.Client();
 bot.login(secret.token); //secret token
 
 bot.on('ready', async guild => {
+    client.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
     console.log(`${bot.user.username} is ready!\nInvite => ${await bot.generateInvite(["ADMINISTRATOR"])}`);
 });
 
-bot.on("guildCreate", async guild => {
+bot.on("guildCreate", async (bot, guild) => {
+    client.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
     let channel = bot.channels.get(guild.systemChannelID);
     if (!channel) return;
     let embed = new Discord.RichEmbed()
       .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
       .setTitle(`I am **${bot.user.username}**, and you better watch your mouth while im in **${guild.name}**`)
       .addField("Made by", "**(Lean)#0108**\nUsing [discord.js](https://discord.js.org) & [request](https://www.npmjs.com/package/request)")
-      .addField("Links", `[GitHub](https://github.com/TasosY2K/SwearBot)\n[Invite](${await bot.generateInvite(["ADMINISTRATOR"])})`)
+      .addField("Links", `[GitHub](https://github.com/TasosY2K/SwearBot)\n[WebSite](https://bot.ly/swear-bot)\n[Invite](${await bot.generateInvite(["ADMINISTRATOR"])})`)
     channel.send(embed);
+});
+
+bot.on("guildCreate", async (bot, guild) => {
+  client.user.setActivity(`${bot.guilds.size} guilds | bit.ly/swear-bot`, {type: 'WATCHING'});
 });
 
 bot.on("message", async message => {
