5d2270b55acd95b17a4035e813d08c87eb11049f
TasosY2K <gustavolama20@gmail.com>
2020-07-04 19:03:55 +0300

    fix: added http server to host on heroku

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const http = require('http');
 const Discord = require('discord.js');
 const {ShardingManager} = require('discord.js');
 const bot = new Discord.Client();
@@ -8,3 +9,5 @@ const secret = process.env;
 const manager = new ShardingManager(__dirname + "/bot.js", {token: secret.TOKEN})
 
 manager.spawn('auto', 2000, true);
+
+http.createServer(onRequest).listen(process.env.PORT || 6000);
